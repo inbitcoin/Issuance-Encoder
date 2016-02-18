c238451b2e50ba074a9f043d47c39ba6f8ef6e21
Oded Leiba <oded@colu.co>
2016-02-18 02:14:22 +0200

    change amountOfUnits to amount

--- test/test_main.js
+++ test/test_main.js
@@ -21,7 +21,7 @@ describe('Test Issuance decoder', function () {
     sha2[21] = 0x23
     sha2[11] = 0x2f
     var data = {
-      amountOfUnits: 1323200,
+      amount: 1323200,
       divisibility: 3,
       lockStatus: false,
       protocol: 0x0302, // Error when start with 0
@@ -43,32 +43,32 @@ describe('Test Issuance decoder', function () {
     console.log(ccEncoding.decode(result.codeBuffer))
 
     data.payments = []
-    data.payments.push({skip: false, range: false, percent: true, output: 12, amountOfUnits: 3213213})
+    data.payments.push({skip: false, range: false, percent: true, output: 12, amount: 3213213})
     result = ccEncoding.encode(data, 40)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
     console.log(ccEncoding.decode(result.codeBuffer))
 
-    data.payments.push({skip: false, range: false, percent: true, output: 1, amountOfUnits: 321321321})
+    data.payments.push({skip: false, range: false, percent: true, output: 1, amount: 321321321})
     result = ccEncoding.encode(data, 40)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
     console.log(ccEncoding.decode(result.codeBuffer))
 
-    data.payments.push({skip: true, range: true, percent: true, output: 1032, amountOfUnits: 1})
+    data.payments.push({skip: true, range: true, percent: true, output: 1032, amount: 1})
     result = ccEncoding.encode(data, 40)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
     console.log(ccEncoding.decode(result.codeBuffer))
 
-    data.payments.push({skip: false, range: false, percent: true, output: 20, amountOfUnits: 100000021000})
+    data.payments.push({skip: false, range: false, percent: true, output: 20, amount: 100000021000})
     result = ccEncoding.encode(data, 40)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
     console.log(ccEncoding.decode(result.codeBuffer))
 
-    data.payments.push({skip: false, range: false, percent: false, output: 0, amountOfUnits: 1})
-    data.payments.push({skip: false, range: false, percent: false, output: 1, amountOfUnits: 2})
-    data.payments.push({skip: true, range: false, percent: false, output: 2, amountOfUnits: 3})
-    data.payments.push({skip: false, range: false, percent: false, output: 3, amountOfUnits: 4})
-    data.payments.push({skip: true, range: false, percent: false, output: 4, amountOfUnits: 5})
-    data.payments.push({skip: false, range: false, percent: false, output: 5, amountOfUnits: 6})
+    data.payments.push({skip: false, range: false, percent: false, output: 0, amount: 1})
+    data.payments.push({skip: false, range: false, percent: false, output: 1, amount: 2})
+    data.payments.push({skip: true, range: false, percent: false, output: 2, amount: 3})
+    data.payments.push({skip: false, range: false, percent: false, output: 3, amount: 4})
+    data.payments.push({skip: true, range: false, percent: false, output: 4, amount: 5})
+    data.payments.push({skip: false, range: false, percent: false, output: 5, amount: 6})
 
     result = ccEncoding.encode(data, 40)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
@@ -76,7 +76,7 @@ describe('Test Issuance decoder', function () {
 
     delete data.noRules
     data.payments = []
-    data.payments.push({skip: false, range: false, percent: true, output: 12, amountOfUnits: 3213213})
+    data.payments.push({skip: false, range: false, percent: true, output: 12, amount: 3213213})
     result = ccEncoding.encode(data, 80)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
     console.log(ccEncoding.decode(result.codeBuffer))
